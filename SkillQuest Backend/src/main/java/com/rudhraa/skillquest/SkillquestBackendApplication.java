2782c4b00b949fd4f94e927b2ec65f32e318385a
Rudhraa Viswanathan <rudhraaviswanathan2@gmail.com>
2026-09-17 12:10:19 +0530

    Complete Sprint 16 backend foundation

--- SkillQuest Backend/src/main/java/com/rudhraa/skillquest/SkillquestBackendApplication.java
+++ SkillQuest Backend/src/main/java/com/rudhraa/skillquest/SkillquestBackendApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SkillquestBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SkillquestBackendApplication.class, args);
+
+        SpringApplication.run(SkillquestBackendApplication.class, args);
 	}
 
 }
